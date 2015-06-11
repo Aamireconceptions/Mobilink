861fc43d13ae1e10d887262f907c81f7907d5b3e
Pehlaj Rai <pehlaj.rai@econceptions.mobi>
2015-06-11 18:02:41 +0500

    Show code verification dialog

--- app/src/main/java/com/ooredoo/bizstore/ui/fragments/SignUpFragment.java
+++ app/src/main/java/com/ooredoo/bizstore/ui/fragments/SignUpFragment.java
@@ -1,9 +1,15 @@
 package com.ooredoo.bizstore.ui.fragments;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 import com.ooredoo.bizstore.R;
+import com.ooredoo.bizstore.ui.activities.BaseActivity;
 import com.ooredoo.bizstore.utils.FragmentUtils;
 
 /**
@@ -24,8 +30,34 @@ public class SignUpFragment extends BaseFragment {
 
     @Override
     public void onClick(View v) {
-        hideKeyboard();
-        AppCompatActivity activity = (AppCompatActivity) mActivity;
-        FragmentUtils.replaceFragmentWithBackStack(activity, R.id.fragment_container, new WelcomeFragment(), "");
+        showVerificationCodeDialog();
     }
+
+
+    public void showVerificationCodeDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mActivity);
+        LayoutInflater inflater = mActivity.getLayoutInflater();
+
+        View view = inflater.inflate(R.layout.dialog_verification_code, null);
+        builder.setView(view);
+
+        final Dialog dialog = builder.create();
+
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        view.findViewById(R.id.btn_next).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                hideKeyboard();
+                AppCompatActivity activity = (AppCompatActivity) mActivity;
+                FragmentUtils.replaceFragmentWithBackStack(activity, R.id.fragment_container, new WelcomeFragment(), "");
+            }
+        });
+
+        dialog.setCancelable(false);
+        builder.setCancelable(false);
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.show();
+    }
+
 }
