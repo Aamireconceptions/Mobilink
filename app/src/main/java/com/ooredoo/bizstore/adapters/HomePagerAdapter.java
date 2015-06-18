c54fade36d88769978e73fe0a5b69984594fe380
Pehlaj Rai <pehlaj.rai@econceptions.mobi>
2015-06-18 17:47:07 +0500

    Add fragments for all tabs.

--- app/src/main/java/com/ooredoo/bizstore/adapters/HomePagerAdapter.java
+++ app/src/main/java/com/ooredoo/bizstore/adapters/HomePagerAdapter.java
@@ -4,85 +4,78 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 
-import com.ooredoo.bizstore.R;
+import com.ooredoo.bizstore.ui.fragments.AutomotiveFragment;
+import com.ooredoo.bizstore.ui.fragments.ElectronicsFragment;
+import com.ooredoo.bizstore.ui.fragments.EntertainmentFragment;
 import com.ooredoo.bizstore.ui.fragments.FoodAndDinningFragment;
 import com.ooredoo.bizstore.ui.fragments.HomeFragment;
+import com.ooredoo.bizstore.ui.fragments.HotelsAndSpasFragment;
+import com.ooredoo.bizstore.ui.fragments.JewelleryFragment;
+import com.ooredoo.bizstore.ui.fragments.MallsFragment;
 import com.ooredoo.bizstore.ui.fragments.ShoppingFragment;
+import com.ooredoo.bizstore.ui.fragments.SportsAndFitnessFragment;
 import com.ooredoo.bizstore.ui.fragments.TopDealsFragment;
+import com.ooredoo.bizstore.ui.fragments.TravelFragment;
 
 /**
  * @author Babar
  * @since 11-Jun-15.
  */
-public class HomePagerAdapter extends FragmentPagerAdapter
-{
-    private final static int PAGE_COUNT = 4;
+public class HomePagerAdapter extends FragmentPagerAdapter {
+    private final static int PAGE_COUNT = 12;
+    public static String[] TAB_NAMES = { "Home", "Top Deals", "Food", "Shopping", "Electronics", "Hotels & Spas", "Malls", "Automotives", "Travel", "Entertainment", "Jewellery", "Sports & Fitness" };
     private AppCompatActivity activity;
 
-    public HomePagerAdapter(AppCompatActivity activity)
-    {
+    public HomePagerAdapter(AppCompatActivity activity) {
         super(activity.getSupportFragmentManager());
-
         this.activity = activity;
     }
 
     @Override
-    public Fragment getItem(int position)
-    {
-        System.out.println("Creating "+position);
+    public Fragment getItem(int position) {
+        System.out.println("Creating " + position);
 
-        switch(position)
-        {
+        switch(position) {
             case 0:
-
                 return HomeFragment.newInstance();
-
             case 1:
-
                 return TopDealsFragment.newInstance();
-
             case 2:
-
                 return FoodAndDinningFragment.newInstance();
-
             case 3:
-
                 return ShoppingFragment.newInstance();
+            case 4:
+                return ElectronicsFragment.newInstance();
+            case 5:
+                return HotelsAndSpasFragment.newInstance();
+            case 6:
+                return MallsFragment.newInstance();
+            case 7:
+                return AutomotiveFragment.newInstance();
+            case 8:
+                return TravelFragment.newInstance();
+            case 9:
+                return EntertainmentFragment.newInstance();
+            case 10:
+                return JewelleryFragment.newInstance();
+            case 11:
+                return SportsAndFitnessFragment.newInstance();
         }
 
         return null;
     }
 
     @Override
-    public int getCount()
-    {
+    public int getCount() {
         return PAGE_COUNT;
     }
 
     @Override
-    public CharSequence getPageTitle(int position)
-    {
-        switch(position)
-        {
-            case 0:
+    public CharSequence getPageTitle(int position) {
 
-                return activity.getString(R.string.home);
-
-            case 1:
-
-                return activity.getString(R.string.top_deals);
-
-            case 2:
-
-                return activity.getString(R.string.food_dining);
-
-            case 3:
-
-                return activity.getString(R.string.shopping);
-
-            default:
-
-                return null;
+        if(position >= 0 && position < TAB_NAMES.length) {
+            return TAB_NAMES[position];
         }
+        return null;
     }
 }
---
+++ app/src/main/java/com/ooredoo/bizstore/ui/fragments/MallsFragment.java
@@ -0,0 +1,83 @@
+package com.ooredoo.bizstore.ui.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.ooredoo.bizstore.R;
+import com.ooredoo.bizstore.adapters.TopDealsAdapter;
+import com.ooredoo.bizstore.model.Deal;
+import com.ooredoo.bizstore.ui.activities.HomeActivity;
+import com.ooredoo.bizstore.utils.ColorUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MallsFragment extends Fragment implements View.OnClickListener {
+    HomeActivity mActivity;
+
+    Button btnNewDeals, btnPopularDeals;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_food_dinning, container, false);
+        mActivity = (HomeActivity) getActivity();
+
+        View header_filters = inflater.inflate(R.layout.header_deals, null);
+
+        ListView listView = (ListView) v.findViewById(R.id.lv);
+
+        btnNewDeals = (Button) header_filters.findViewById(R.id.btn_new_deals);
+        btnPopularDeals = (Button) header_filters.findViewById(R.id.btn_popular_deals);
+
+        btnNewDeals.setOnClickListener(this);
+        btnPopularDeals.setOnClickListener(this);
+
+        listView.addHeaderView(header_filters);
+
+        init(v);
+
+        return v;
+    }
+
+    private void init(View v) {
+        List<Deal> deals = new ArrayList<>();
+        deals.add(new Deal());
+        deals.add(new Deal());
+        deals.add(new Deal());
+        deals.add(new Deal());
+        deals.add(new Deal());
+        deals.add(new Deal());
+        ListView listView = (ListView) v.findViewById(R.id.lv);
+        TopDealsAdapter adapter = new TopDealsAdapter(mActivity, R.layout.list_item_food_n_dining, deals);
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if(id == R.id.btn_new_deals || id == R.id.btn_popular_deals) {
+            btnNewDeals.setTextColor(id == R.id.btn_new_deals ? ColorUtils.WHITE : ColorUtils.BLACK);
+            btnPopularDeals.setTextColor(id == R.id.btn_new_deals ? ColorUtils.BLACK : ColorUtils.WHITE);
+            btnNewDeals.setBackgroundResource(id == R.id.btn_new_deals ? R.drawable.btn_red1 : R.drawable.btn_lt_grey1);
+            btnPopularDeals.setBackgroundResource(id == R.id.btn_new_deals ? R.drawable.btn_lt_grey2 : R.drawable.btn_red2);
+        }
+    }
+
+    public static MallsFragment newInstance() {
+        MallsFragment fragment = new MallsFragment();
+
+        return fragment;
+    }
+
+}
