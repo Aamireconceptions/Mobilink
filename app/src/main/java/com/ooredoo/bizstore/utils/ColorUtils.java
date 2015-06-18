9d352aad618074093735fa1b881f4f8aac8f5b42
Pehlaj Rai <pehlaj.rai@econceptions.mobi>
2015-06-18 16:29:55 +0500

    Add Utility class i.e. ColorUtils

---
+++ app/src/main/java/com/ooredoo/bizstore/utils/ColorUtils.java
@@ -0,0 +1,15 @@
+package com.ooredoo.bizstore.utils;
+
+import android.graphics.Color;
+
+/**
+ * @author Pehlaj Rai
+ * @since 1/2/2015.
+ */
+
+public class ColorUtils {
+    public static final int LT_GRAY = Color.parseColor("#f2f2f2");
+    public static final int RED = Color.parseColor("#ec1b24");
+    public static final int WHITE = Color.parseColor("#FFFFFF");
+    public static final int BLACK = Color.parseColor("#000000");
+}
