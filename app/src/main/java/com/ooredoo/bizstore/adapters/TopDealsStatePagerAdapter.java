f48d817b8810a17d189f20bc733cc02077f45fe8
Raja Babar <raja.babar@econceptions.mobi>
2015-06-19 12:25:18 +0500

    Working on TopDealsStatePagerAdapter

--- app/src/main/java/com/ooredoo/bizstore/adapters/TopDealsStatePagerAdapter.java
+++ app/src/main/java/com/ooredoo/bizstore/adapters/TopDealsStatePagerAdapter.java
@@ -1,6 +1,7 @@
 package com.ooredoo.bizstore.adapters;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.support.v13.app.FragmentStatePagerAdapter;
 
 /**
@@ -9,6 +10,10 @@ import android.support.v13.app.FragmentStatePagerAdapter;
  */
 public class TopDealsStatePagerAdapter extends FragmentStatePagerAdapter
 {
+
+    public TopDealsStatePagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
 
     @Override
     public Fragment getItem(int position) {
