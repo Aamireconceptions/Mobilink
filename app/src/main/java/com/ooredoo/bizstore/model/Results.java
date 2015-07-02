a572c287b98fb9a95a5318e688b762c1f6ab93b8
Pehlaj Rai <pehlaj.rai@econceptions.mobi>
2015-07-02 13:15:12 +0500

    Update search task.

---
+++ app/src/main/java/com/ooredoo/bizstore/model/Results.java
@@ -0,0 +1,18 @@
+package com.ooredoo.bizstore.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * @author Pehlaj Rai.
+ * @since 03/20/2014.
+ */
+public class Results {
+
+    @SerializedName("result")
+    public int resultCode;
+
+    @SerializedName("results")
+    public List<Deal> list;
+}
