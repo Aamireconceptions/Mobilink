a9ebcea44a78ec6f211d54fd95c1a0e2b2e5e9d9
Pehlaj Rai <pehlaj.rai@econceptions.mobi>
2015-07-07 11:04:10 +0500

    Update sub category names

--- app/src/main/java/com/ooredoo/bizstore/utils/CategoryUtils.java
+++ app/src/main/java/com/ooredoo/bizstore/utils/CategoryUtils.java
@@ -36,55 +36,55 @@ public class CategoryUtils {
 
         /* FOOD & DINING */
         subCategories.add(new SubCategory(1, R.id.cb_food_arabic, "Arabic", "food_arabic", CT_FOOD, false, false));
-        subCategories.add(new SubCategory(2, R.id.cb_food_turkish, "Arabic", "food_turkish", CT_FOOD, false, false));
-        subCategories.add(new SubCategory(3, R.id.cb_food_indian, "Arabic", "food_indian", CT_FOOD, false, false));
-        subCategories.add(new SubCategory(4, R.id.cb_food_chinese, "Arabic", "food_chinese", CT_FOOD, false, false));
-        subCategories.add(new SubCategory(5, R.id.cb_food_cafes, "Arabic", "food_cafes", CT_FOOD, false, false));
-        subCategories.add(new SubCategory(6, R.id.cb_fast_food, "Arabic", "food_fast_food", CT_FOOD, false, false));
-        subCategories.add(new SubCategory(7, R.id.cb_food_italian, "Arabic", "food_italian", CT_FOOD, false, false));
-        subCategories.add(new SubCategory(8, R.id.cb_food_afghani, "Arabic", "food_afghani", CT_FOOD, false, false));
-        subCategories.add(new SubCategory(9, R.id.cb_food_lebanese, "Arabic", "food_lebanese", CT_FOOD, false, false));
-        subCategories.add(new SubCategory(10, R.id.cb_food_sweets, "Arabic", "food_sweets", CT_FOOD, false, false));
+        subCategories.add(new SubCategory(2, R.id.cb_food_turkish, "Turkish", "food_turkish", CT_FOOD, false, false));
+        subCategories.add(new SubCategory(3, R.id.cb_food_indian, "Indian", "food_indian", CT_FOOD, false, false));
+        subCategories.add(new SubCategory(4, R.id.cb_food_chinese, "Chinese", "food_chinese", CT_FOOD, false, false));
+        subCategories.add(new SubCategory(5, R.id.cb_food_cafes, "Cafes", "food_cafes", CT_FOOD, false, false));
+        subCategories.add(new SubCategory(6, R.id.cb_fast_food, "Fast Food", "food_fast_food", CT_FOOD, false, false));
+        subCategories.add(new SubCategory(7, R.id.cb_food_italian, "Italian", "food_italian", CT_FOOD, false, false));
+        subCategories.add(new SubCategory(8, R.id.cb_food_afghani, "Afghani", "food_afghani", CT_FOOD, false, false));
+        subCategories.add(new SubCategory(9, R.id.cb_food_lebanese, "Lebanese", "food_lebanese", CT_FOOD, false, false));
+        subCategories.add(new SubCategory(10, R.id.cb_food_sweets, "Sweets", "food_sweets", CT_FOOD, false, false));
 
         /* SHOPPING */
-        subCategories.add(new SubCategory(11, R.id.cb_apparel, "Arabic", "shopping_apparel", CT_SHOPPING, false, false));
-        subCategories.add(new SubCategory(12, R.id.cb_home_goods, "Arabic", "shopping_home_goods", CT_SHOPPING, false, false));
-        subCategories.add(new SubCategory(13, R.id.cb_sports, "Arabic", "shopping_sports_fitness", CT_SHOPPING, false, false));
-        subCategories.add(new SubCategory(14, R.id.cb_shop_electronics, "Arabic", "shopping_electronics", CT_SHOPPING, false, false));
-        subCategories.add(new SubCategory(15, R.id.cb_fashion, "Arabic", "shopping_fashion", CT_SHOPPING, false, false));
+        subCategories.add(new SubCategory(11, R.id.cb_apparel, "Apparel", "shopping_apparel", CT_SHOPPING, false, false));
+        subCategories.add(new SubCategory(12, R.id.cb_home_goods, "Home Goods", "shopping_home_goods", CT_SHOPPING, false, false));
+        subCategories.add(new SubCategory(13, R.id.cb_sports, "Sports & Fitness", "shopping_sports_fitness", CT_SHOPPING, false, false));
+        subCategories.add(new SubCategory(14, R.id.cb_shop_electronics, "Electronics", "shopping_electronics", CT_SHOPPING, false, false));
+        subCategories.add(new SubCategory(15, R.id.cb_fashion, "Fashion", "shopping_fashion", CT_SHOPPING, false, false));
 
         /* ELECTRONICS */
-        subCategories.add(new SubCategory(16, R.id.cb_tv, "Arabic", "electronics_tv_home", CT_ELECTRONICS, false, false));
-        subCategories.add(new SubCategory(17, R.id.cb_computers, "Arabic", "electronics_computer_tablets", CT_ELECTRONICS, false, false));
-        subCategories.add(new SubCategory(18, R.id.cb_camera_photo, "Arabic", "electronics_cameras_photography", CT_ELECTRONICS, false, false));
-        subCategories.add(new SubCategory(19, R.id.cb_mobiles, "Arabic", "electronics_mobile", CT_ELECTRONICS, false, false));
+        subCategories.add(new SubCategory(16, R.id.cb_tv, "TV", "electronics_tv_home", CT_ELECTRONICS, false, false));
+        subCategories.add(new SubCategory(17, R.id.cb_computers, "Computers & Tablets", "electronics_computer_tablets", CT_ELECTRONICS, false, false));
+        subCategories.add(new SubCategory(18, R.id.cb_camera_photo, "Cameras & Photography", "electronics_cameras_photography", CT_ELECTRONICS, false, false));
+        subCategories.add(new SubCategory(19, R.id.cb_mobiles, "Mobiles", "electronics_mobile", CT_ELECTRONICS, false, false));
 
         /* HOTELS & SPAS */
-        subCategories.add(new SubCategory(20, R.id.cb_salons, "Arabic", "hotels_salons", CT_HOTELS, false, false));
-        subCategories.add(new SubCategory(21, R.id.cb_logding, "Arabic", "hotels_lodging", CT_HOTELS, false, false));
-        subCategories.add(new SubCategory(22, R.id.cb_spas, "Arabic", "hotels_spas", CT_HOTELS, false, false));
+        subCategories.add(new SubCategory(20, R.id.cb_salons, "Salon", "hotels_salons", CT_HOTELS, false, false));
+        subCategories.add(new SubCategory(21, R.id.cb_logding, "Logding", "hotels_lodging", CT_HOTELS, false, false));
+        subCategories.add(new SubCategory(22, R.id.cb_spas, "Spas", "hotels_spas", CT_HOTELS, false, false));
 
         /* MARKETS & MALLS */
-        subCategories.add(new SubCategory(23, R.id.cb_hypermarkets, "Arabic", "markets_hypermarkets", CT_MALLS, false, false));
-        subCategories.add(new SubCategory(24, R.id.cb_malls, "Arabic", "markets_malls", CT_MALLS, false, false));
+        subCategories.add(new SubCategory(23, R.id.cb_hypermarkets, "Hypermarkets", "markets_hypermarkets", CT_MALLS, false, false));
+        subCategories.add(new SubCategory(24, R.id.cb_malls, "Malls", "markets_malls", CT_MALLS, false, false));
 
         /* AUTOMOTIVE */
-        subCategories.add(new SubCategory(25, R.id.cb_showrooms, "Arabic", "automotives_showroom", CT_AUTOMOTIVE, false, false));
-        subCategories.add(new SubCategory(26, R.id.cb_auto_accessories, "Arabic", "automotives_accessories", CT_AUTOMOTIVE, false, false));
-        subCategories.add(new SubCategory(27, R.id.cb_auto_services, "Arabic", "automotives_services", CT_AUTOMOTIVE, false, false));
+        subCategories.add(new SubCategory(25, R.id.cb_showrooms, "Show Rooms", "automotives_showroom", CT_AUTOMOTIVE, false, false));
+        subCategories.add(new SubCategory(26, R.id.cb_auto_accessories, "Accessories", "automotives_accessories", CT_AUTOMOTIVE, false, false));
+        subCategories.add(new SubCategory(27, R.id.cb_auto_services, "Services", "automotives_services", CT_AUTOMOTIVE, false, false));
 
         /* ENTERTAINMENT */
-        subCategories.add(new SubCategory(28, R.id.cb_events, "Arabic", "entertainment_events", CT_ENTERTAINMENT, false, false));
-        subCategories.add(new SubCategory(29, R.id.cb_kids_activities, "Arabic", "entertainment_kids_activities", CT_ENTERTAINMENT, false, false));
-        subCategories.add(new SubCategory(30, R.id.cb_cinemas, "Arabic", "entertainment_cinemas", CT_ENTERTAINMENT, false, false));
+        subCategories.add(new SubCategory(28, R.id.cb_events, "Events", "entertainment_events", CT_ENTERTAINMENT, false, false));
+        subCategories.add(new SubCategory(29, R.id.cb_kids_activities, "Kids Activities", "entertainment_kids_activities", CT_ENTERTAINMENT, false, false));
+        subCategories.add(new SubCategory(30, R.id.cb_cinemas, "Cinemas", "entertainment_cinemas", CT_ENTERTAINMENT, false, false));
 
         /* JEWELLERY & EXCHANGE */
-        subCategories.add(new SubCategory(31, R.id.cb_gold_rate, "Arabic", "jewelry_gold", CT_JEWELLERY, false, false));
-        subCategories.add(new SubCategory(32, R.id.cb_currency, "Arabic", "jewelry_currency", CT_JEWELLERY, false, false));
+        subCategories.add(new SubCategory(31, R.id.cb_gold_rate, "Gold", "jewelry_gold", CT_JEWELLERY, false, false));
+        subCategories.add(new SubCategory(32, R.id.cb_currency, "Currency", "jewelry_currency", CT_JEWELLERY, false, false));
 
         /* SPORTS & FITNESS */
-        subCategories.add(new SubCategory(33, R.id.cb_sports_clothing, "Arabic", "sports_clothing", CT_SPORTS, false, false));
-        subCategories.add(new SubCategory(34, R.id.cb_sports_equipment, "Arabic", "sports_equipment", CT_SPORTS, false, false));
+        subCategories.add(new SubCategory(33, R.id.cb_sports_clothing, "Clothing", "sports_clothing", CT_SPORTS, false, false));
+        subCategories.add(new SubCategory(34, R.id.cb_sports_equipment, "Equipment", "sports_equipment", CT_SPORTS, false, false));
     }
 
     public static synchronized void showSubCategories(Activity activity, int category) {
