25d5f4f227822a276404e105f89bd1e656551ee9
Pehlaj Rai <pehlaj.rai@econceptions.mobi>
2015-08-07 12:02:57 +0500

    Reset tab filters on tab un-select

--- app/src/main/java/com/ooredoo/bizstore/listeners/HomeTabSelectedListener.java
+++ app/src/main/java/com/ooredoo/bizstore/listeners/HomeTabSelectedListener.java
@@ -5,6 +5,7 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 
 import com.ooredoo.bizstore.R;
+import com.ooredoo.bizstore.asynctasks.DealsTask;
 import com.ooredoo.bizstore.ui.activities.HomeActivity;
 import com.ooredoo.bizstore.utils.Logger;
 
@@ -33,8 +34,9 @@ public class HomeTabSelectedListener implements TabLayout.OnTabSelectedListener
 
         viewPager.setCurrentItem(tab.getPosition(), true);
 
-        /*DealsTask.sortColumn = "createdate";
-        DealsTask.subCategories = "";*/
+        DealsTask.sortColumn = "createdate";
+        DealsTask.subCategories = "";
+
         setCurrentFragment();
     }
 
