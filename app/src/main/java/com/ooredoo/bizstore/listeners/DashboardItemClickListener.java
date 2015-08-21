54a391fb7995869ec3b9dcb1bc84ed7055451a08
Pehlaj Rai <pehlaj.rai@econceptions.mobi>
2015-08-21 13:20:15 +0500

    Fix dashboard item selection issue

--- app/src/main/java/com/ooredoo/bizstore/listeners/DashboardItemClickListener.java
+++ app/src/main/java/com/ooredoo/bizstore/listeners/DashboardItemClickListener.java
@@ -3,10 +3,17 @@ package com.ooredoo.bizstore.listeners;
 import android.view.View;
 
 import com.ooredoo.bizstore.R;
+import com.ooredoo.bizstore.asynctasks.DealsTask;
 import com.ooredoo.bizstore.interfaces.OnFilterChangeListener;
 import com.ooredoo.bizstore.ui.activities.HomeActivity;
 import com.ooredoo.bizstore.ui.fragments.EntertainmentFragment;
 import com.ooredoo.bizstore.ui.fragments.HotelsAndSpasFragment;
+
+import static com.ooredoo.bizstore.utils.CategoryUtils.getCategoryCheckboxId;
+import static com.ooredoo.bizstore.utils.CategoryUtils.getCategoryFilter;
+import static com.ooredoo.bizstore.utils.CategoryUtils.getParentCategory;
+import static com.ooredoo.bizstore.utils.CategoryUtils.resetSubCategories;
+import static com.ooredoo.bizstore.utils.CategoryUtils.updateSubCategorySelection;
 
 /**
  * @author Pehlaj Rai
@@ -70,17 +77,40 @@ public class DashboardItemClickListener implements View.OnClickListener {
             if(id == R.id.salons || id == R.id.lodging || id == R.id.spas) {
                 String subCategory = id == R.id.salons ? "salons" : id == R.id.lodging ? "lodging" : "spas";
 
-                HotelsAndSpasFragment.subCategory = "hotels_spas" + subCategory;
+                HotelsAndSpasFragment.subCategory = "hotels_" + subCategory;
+
+                processSubCategory(subCategory);
+
                 mActivity.selectTab(5);
             }
 
             if(id == R.id.events || id == R.id.movie_tickets || id == R.id.kids_activities) {
                 String subCategory = id == R.id.events ? "events" : id == R.id.movie_tickets ? "cinemas" : "kids_activities";
                 EntertainmentFragment.subCategory = "entertainment_" + subCategory;
+                String subCategoryName = getEntertainmentSubCategory(id);
+                processSubCategory(subCategoryName);
                 mActivity.selectTab(9);
             }
 
             onFilterChangeListener.onFilterChange();
         }
+    }
+
+    private String getEntertainmentSubCategory(int id) {
+        return id == R.id.events ? mActivity.getString(R.string.events) : id == R.id.movie_tickets ? mActivity.getString(R.string.movie_tickets) : mActivity.getString(R.string.Kids_Activities);
+    }
+
+    private void processSubCategory(String subCategoryName) {
+        String filter = getCategoryFilter(subCategoryName);
+
+        int category = getParentCategory(subCategoryName);
+
+        resetSubCategories(category);
+
+        int subCategoryCheckboxId = getCategoryCheckboxId(subCategoryName);
+
+        updateSubCategorySelection(subCategoryCheckboxId, true);
+
+        DealsTask.subCategories = filter;
     }
 }
